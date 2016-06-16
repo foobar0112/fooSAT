c4f3d23a2fdadfb8b6b6f5cf77799ae70e9c4fe7
foobar0112 <foobar2@posteo.de>
2016-06-16 19:56:24 +0200

    initial commit \o/

---
+++ foo/Parse.cc
@@ -0,0 +1,159 @@
+//
+// Created by hejn on 29.05.16.
+//
+
+#include "Parse.h"
+
+using namespace std;
+using namespace Minisat;
+
+namespace fooSAT {
+
+    static int generateInput(const char *path, stringstream *cnf, vector<string> *rev_mapper) {
+
+        set<string> edges;
+        int num_vert = readGraphFile(path, &edges);
+
+        //ofstream cnf(cnf_path, ofstream::out);
+
+        *cnf << "p cnf " << getNumVars(num_vert) << " " << getNumClaus(num_vert) << endl;
+
+        map<string, int> mapper;
+        for (int i = 0; i < num_vert; i++) {
+            for (int j = 0; j < num_vert; j++) {
+                string s = "p " + to_string(j) + " " + to_string(i);
+                mapper.insert(make_pair(s, mapper.size() + 1));
+                rev_mapper->push_back(s);
+            }
+        }
+        for (int i = 0; i < num_vert; i++) {
+            for (int j = 0; j < num_vert; j++) {
+                if (i != j) {
+                    string s = "e " + to_string(i) + " " + to_string(j);
+                    mapper.insert(make_pair(s, mapper.size() + 1));
+                    rev_mapper->push_back(s);
+                }
+            }
+        }
+
+        vector<string> lits;
+
+        for (int i = 0; i < num_vert; i++) {
+            lits.clear();
+            for (int j = 0; j < num_vert; j++) {
+                lits.push_back("p " + to_string(i) + " " + to_string(j));
+            }
+            appendClaus(cnf, lits, &mapper);
+        }
+
+        for (int i = 0; i < num_vert; i++) {
+            for (int j = 0; j < num_vert; j++) {
+                for (int k = 0; k < num_vert; k++) {
+                    if (k != j)
+                        appendClaus(cnf, {"-p " + to_string(j) + " " + to_string(i),
+                                           "-p " + to_string(k) + " " + to_string(i)}, &mapper);
+                }
+            }
+        }
+
+        for (int i = 0; i < num_vert; i++) {
+            for (int j = 0; j < num_vert; j++) {
+                for (int k = 0; k < num_vert; k++) {
+                    if (j != i)
+                        appendClaus(cnf, {"e " + to_string(i) + " " + to_string(j),
+                                           "-p " + to_string(k) + " " + to_string(i),
+                                           "-p " + to_string((k + 1) % num_vert) + " " + to_string(j)}, &mapper);
+                }
+            }
+        }
+
+        for (int i = 0; i < num_vert; i++) {
+            for (int j = 0; j < num_vert; j++) {
+                if (i != j) {
+                    string s = edges.find(to_string(i) + " " + to_string(j)) != edges.end() ? "" : "-";
+                    s += "e " + to_string(i) + " " + to_string(j);
+                    appendClaus(cnf, {s}, &mapper);
+                }
+            }
+        }
+
+        //cnf.close();
+        return num_vert * num_vert;
+    }
+
+    static void appendClaus(stringstream *cnf, vector<string> lits, map<string, int> *mapper) {
+        for (string l :lits) {
+            if (l[0] == '-') {
+                *cnf << "-" + to_string(mapper->find(l.erase(0, 1))->second) << " ";
+            } else {
+                *cnf << to_string(mapper->find(l)->second) << " ";
+            }
+        }
+        *cnf << "0" << endl;
+    }
+
+    static int getNumClaus(int num_vert) { return 2 * num_vert * num_vert * num_vert - num_vert * num_vert; }
+
+    static int getNumVars(int num_vert) { return 2 * num_vert * num_vert - num_vert; }
+
+    static int readGraphFile(const char *path, set<string> *edges) {
+
+        int vert = 0;
+
+        ifstream graph(path);
+
+        char e;
+        int i, j;
+
+        string line;
+        while (getline(graph, line)) {
+            string f;
+            istringstream iss(line);
+            if ((iss >> e >> f >> i >> j)) {
+                if (e == 'p') {
+                    vert = i;
+                    //printf("#vert = %d\n", vert);
+                    break;
+                }
+            }
+        }
+
+        while (getline(graph, line)) {
+            istringstream iss(line);
+            if ((iss >> e >> i >> j)) {
+                if (e == 'e') {
+                    addEdge(edges, i-1, j-1);
+                }
+            }
+        }
+
+        graph.close();
+
+        return vert;
+    }
+
+    static void addEdge(set<string> *edges, int i, int j) {
+        edges->insert(to_string(i) + " " + to_string(j));
+        //printf("%d -> %d\n", i, j);
+    }
+
+    void interpretResult(vec<lbool> *model, int nVars, vector<string> mapper) {
+
+        set<pair<int, int>, paircomp> s;
+        for (int i = 0; i < nVars; ++i) {
+            if ((*model)[i] == l_True) {
+                istringstream iss(mapper[i]);
+                vector<string> parts{istream_iterator<string> {iss},
+                                     istream_iterator<string> {}};
+                int j = stoi(parts[1]);
+                int k = stoi(parts[2]);
+                s.insert(make_pair(j, k));
+            }
+        }
+        for (auto f : s) {
+            printf("%d ", f.second + 1);
+        }
+
+    }
+
+}
---
+++ foo/Parse.h
@@ -0,0 +1,48 @@
+//
+// Created by hejn on 29.05.16.
+//
+
+#ifndef FOOSAT_PARSE_H
+#define FOOSAT_PARSE_H
+
+#include <stdio.h>
+#include <set>
+#include <vector>
+#include <map>
+#include <utility>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <iostream>
+
+#include "../minisat/utils/ParseUtils.h"
+#include "../minisat/mtl/Vec.h"
+#include "../minisat/core/SolverTypes.h"
+
+using namespace std;
+using namespace Minisat;
+
+namespace fooSAT {
+
+    struct paircomp {
+        bool operator()(const pair<int, int> &lhs, const pair<int, int> &rhs) const {
+            return (lhs.second < rhs.second || lhs.first < rhs.first);
+        }
+    };
+
+    static void appendClaus(stringstream *cnf, vector<string> lits, map<string, int> *mapper);
+
+    static int getNumClaus(int num_vert);
+
+    static int getNumVars(int num_vert);
+
+    static int generateInput(const char *path, stringstream *cnf, vector<string> *rev_mapper);
+
+    static int readGraphFile(const char *path, set<string> *edges);
+
+    static void addEdge(set<string> *edge, int i, int j);
+
+    static void interpretResult(vec<lbool> *model, int numVars, vector<string> mapper);
+}
+
+#endif //FOOSAT_PARSE_H
